ef90332887e5d6e169728c310bd3de492e1d59f4
EC2 Default User <ec2-user@ip-172-31-2-240.us-west-2.compute.internal>
2022-06-20 16:28:13 +0000

    Bootstrapping ArgoCD

--- lib/pipeline.ts
+++ lib/pipeline.ts
@@ -21,6 +21,34 @@ export default class PipelineConstruct extends Construct {
     )
     .teams(new TeamPlatform(account), new TeamApplication('burnham',account));
   
+    // HERE WE ADD THE ARGOCD APP OF APPS REPO INFORMATION
+    const repoUrl = 'https://github.com/aws-samples/eks-blueprints-workloads.git';
+
+    const bootstrapRepo : blueprints.ApplicationRepository = {
+        repoUrl,
+        targetRevision: 'workshop',
+    }
+
+    // HERE WE GENERATE THE ADDON CONFIGURATIONS
+    const devBootstrapArgo = new blueprints.ArgoCDAddOn({
+        bootstrapRepo: {
+            ...bootstrapRepo,
+            path: 'envs/dev'
+        },
+    });
+    const testBootstrapArgo = new blueprints.ArgoCDAddOn({
+        bootstrapRepo: {
+            ...bootstrapRepo,
+            path: 'envs/test'
+        },
+    });
+    const prodBootstrapArgo = new blueprints.ArgoCDAddOn({
+        bootstrapRepo: {
+            ...bootstrapRepo,
+            path: 'envs/prod'
+        },
+    });
+    
     blueprints.CodePipelineStack.builder()
       .name("eks-blueprints-workshop-pipeline")
       .owner("shreyarm-aws")
@@ -32,9 +60,9 @@ export default class PipelineConstruct extends Construct {
       .wave({
         id: "envs",
         stages: [
-          { id: "dev", stackBuilder: blueprint.clone('us-west-2')},
-          { id: "test", stackBuilder: blueprint.clone('us-east-2')},
-          { id: "prod", stackBuilder: blueprint.clone('us-east-1')}
+          { id: "dev", stackBuilder: blueprint.clone('us-west-2').addOns(devBootstrapArgo)},
+          { id: "test", stackBuilder: blueprint.clone('us-east-2').addOns(devBootstrapArgo)},
+          { id: "prod", stackBuilder: blueprint.clone('us-east-1').addOns(devBootstrapArgo)}
         ]
       })
       .build(scope, id+'-stack', props);
